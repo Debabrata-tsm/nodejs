9f24af3e556dc0eb8f588cf162386e3fd9d0c890
Debabrata-tsm <debabrata@thinksurfmedia.info>
2024-09-25 18:45:21 +0530

    Update deploy.sh

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,47 +1,19 @@
 #!/bin/bash
-
 set -e
-echo "Deployment started"
 
-# Set environment variables
-PROJECT_PATH=${PROJECT_PATH:-"/usr/share/nginx/node"}
-PM2_APP_NAME=${PM2_APP_NAME:-"app1"}  # Define your PM2 app name
+echo "Deployment started..."
 
-# Load NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-source ~/.bashrc
+# Pull the latest version of the app
+git pull origin master
+echo "New changes copied to server !"
 
-# Use the desired Node.js version
-nvm use 16  # This uses the default version. You can specify a version like: nvm use 14
-
-# Navigate to the project directory
-cd $PROJECT_PATH || exit
-
-# Log Node.js version for reference
-node -v
-
-git pull origin main
-
-# Install project dependencies
+echo "Installing Dependencies..."
 npm install --yes
 
-# Restart the app with PM2 or start it if not running using the ecosystem file
-pm2 restart ecosystem.config.js || pm2 start ecosystem.config.js
+echo "Creating Production Build..."
+npm run build
 
-# Configure PM2 to start on boot
-pm2 startup  # This generates the startup command for your system
+echo "PM2 Reload"
+pm2 reload app1
 
-# Get the generated startup command and run it
-STARTUP_CMD=$(pm2 startup | grep 'sudo' | tail -1)
-eval $STARTUP_CMD
-
-# Save the current PM2 process list to be started after reboot
-pm2 save
-
-# Optional: Log deployment time or output
-echo "Deployment completed at $(date)" >> deployment.log
+echo "Deployment Finished!"
